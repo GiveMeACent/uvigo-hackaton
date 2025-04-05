50461176556009ef8eea1884674e7ef3642d2246
johnpberenger <johnpberenger@gmail.com>
2025-04-05 11:30:03 +0200

    Update filestruct.py

--- filestruct.py
+++ filestruct.py
@@ -1,5 +1,4 @@
 import os
-from datetime import datetime
 
 def createFileStruct(device_name: str):
     base_path = os.path.expanduser("~/media") # home directory + "~/media" = base path
@@ -9,5 +8,6 @@ def createFileStruct(device_name: str):
     os.makedirs(os.path.join(device_folder, "videos"), exist_ok=True)
     os.makedirs(os.path.join(device_folder, "gcsv"), exist_ok=True)     # videos, gcsv subfolders
 
-    print(f"[INFO] Created folders for {device_name} at {device_folder}")
+    print(f"Created folders for {device_name} at {device_folder}")
     return device_folder
+
