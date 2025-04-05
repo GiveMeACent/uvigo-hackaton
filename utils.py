4ce164082e246c47661b77cd453e9cdb3a2a476b
giuseppe1-1 <signorelligiuseppe81@gmail.com>
2025-04-05 11:53:47 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,30 +1,24 @@
+# from usbmonitor import USBMonitor
+# from usbmonitor.attributes import ID_MODEL, ID_MODEL_ID, ID_VENDOR_ID
 import json
-from usbmonitor import USBMonitor
-from usbmonitor.attributes import ID_MODEL, ID_MODEL_ID, ID_VENDOR_ID
+import subprocess
 
+def detectDevices() -> list:
+    result = subprocess.run(['lsusb'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    if result.returncode == 0:
+        devices = result.stdout.splitlines()
+        return devices
+    else:
+        return f"Errore lsusb: {result.stderr}"
 
-def detectDevices() -> dict: 
-  # Create the USBMonitor instance
-  monitor = USBMonitor()
-
-  # Get the current devices
-  devicesDict = monitor.get_available_devices()
-
-  # for device_id, device_info in devicesDict.items():
-  #     print(f"DEVICE ID: {device_id} DEVICE MODEL: {device_info[ID_MODEL]} DEVICE ID MODEL ID: ({device_info[ID_MODEL_ID]} - DEVICE INFO: {device_info[ID_VENDOR_ID]})")
-  
-  return devicesDict
-
-def checkCamera(devicesDict: dict) -> bool:
+def checkCamera(devices: list) -> bool:
   found = False
   with open("config.json") as f:
     d = json.load(f)
     supported_camera = d["supported_cameras"][0]
-    for device_id, device_info in devicesDict.items() :
-      if (supported_camera["device_id"] == device_id and supported_camera["device_model"] == device_info[ID_MODEL] and supported_camera["device_model_id_model"] == device_info[ID_MODEL_ID] and supported_camera["device_info"] == device_info[ID_VENDOR_ID]) :
-        found = True
-        print(f"Supported camera connected: {model_id} ({vendor_id})")
-        createFileStruct(model_id)
-        break
+    for device in devices :
+       id = device.split()[5]
+       if (id == supported_camera["id"]):
+          found = True
   
   return found
