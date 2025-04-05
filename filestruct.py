c050acf7af699d3c614c5c4654c13bad74bd5203
johnpberenger <johnpberenger@gmail.com>
2025-04-05 11:03:11 +0200

    Update filestruct.py

--- filestruct.py
+++ filestruct.py
@@ -2,14 +2,12 @@ import os
 from datetime import datetime
 
 def create_file_structure(device_name: str):
-    base_path = os.path.expanduser("~/media") # user's home directory and ~/media as the base path
-
-
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    device_folder = os.path.join(base_path, f"{device_name}_{timestamp}")     # Create a unique folder name based on the device name and current timestamp
+    base_path = os.path.expanduser("~/media") # home directory + ~/media = base path
+    
+    device_folder = os.path.join(base_path, f"{device_name}")     
 
     os.makedirs(os.path.join(device_folder, "videos"), exist_ok=True)
-    os.makedirs(os.path.join(device_folder, "gcsv"), exist_ok=True)     # Create 'videos' and 'gcsv' subfolders inside the device folder
+    os.makedirs(os.path.join(device_folder, "gcsv"), exist_ok=True)     # videos, gcsv subfolders
 
     print(f"[INFO] Created folders for {device_name} at {device_folder}")
     return device_folder
